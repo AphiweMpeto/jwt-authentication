8a65efb07109d2945a0c985738a8b098b3766a80
AphiweMp <aphiwempeto@gmail.com>
2022-02-18 10:57:39 +0200

    completed backed

---
+++ app/config/db.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  HOST: "sql211.byetcluster.com",
+  USER: "epiz_31096553",
+  PASSWORD: "BmS7i7iXAv1hD",
+  DB: "epiz_31096553_alexsexwale",
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+};
